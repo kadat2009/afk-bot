5bedf9b0e23c912166c47cecb8bb7d904fcb2578
kadat2009 <ggfddggff790@gmail.com>
2026-05-11 10:49:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function createBot() {
     port: 27720,
     username: 'AFK_Bot',
     auth: 'offline',
-    version: '1.21.1' // 🔥 QUAN TRỌNG
+    version: '1.21.11'
   })
 
   bot.on('login', () => console.log('Bot logged in'))
