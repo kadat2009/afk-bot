922026cd6aaa16b209cc724c27a60bba3661038a
kadat2009 <ggfddggff790@gmail.com>
2026-05-11 10:41:51 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'sinhtonminecraft100.aternos.me',
+    port: 27720,
+    username: 'AFK_Bot',
+    auth: 'offline'
+  })
+
+  bot.on('login', () => console.log('Bot online'))
+
+  bot.on('spawn', () => console.log('Bot spawned'))
+
+  setInterval(() => {
+    const actions = ['jump', 'forward', 'back']
+    const action = actions[Math.floor(Math.random() * actions.length)]
+
+    bot.setControlState(action, true)
+    setTimeout(() => bot.setControlState(action, false), 2000)
+  }, 30000)
+
+  bot.on('end', () => {
+    console.log('Disconnected → reconnect')
+    setTimeout(createBot, 10000)
+  })
+
+  bot.on('error', console.log)
+}
+
+createBot()
